5b548db273dc683d2cbe2d5ff534753427369dab
ucyo <cayoglu@me.com>
2020-09-26 18:04:01 +0200

    Add necessary traits for data model to change as less code as possible

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -2,7 +2,7 @@
 
 pub mod models {
     use serde::{Deserialize, Serialize};
-    #[derive(Debug, Serialize, Default, Deserialize, Clone)]
+    #[derive(Debug, Serialize, Default, Deserialize, Clone, Copy)]
     pub struct Data {
         pub value: i64,
         id: usize,
@@ -11,6 +11,37 @@ pub mod models {
     impl Data {
         pub fn new(value: i64, id: usize) -> Self {
             Data { value, id }
+        }
+        pub fn update(&mut self, value: i64) {
+            self.value = value
+        }
+    }
+
+    use std::ops::{SubAssign, AddAssign};
+    impl SubAssign<i64> for Data {
+        fn sub_assign(&mut self, rhs: i64) {
+            self.value -= rhs;
+        }
+    }
+    impl AddAssign<i64> for Data {
+        fn add_assign(&mut self, rhs: i64) {
+            self.value += rhs
+        }
+    }
+    use std::fmt::{Formatter, Result, Display};
+    impl Display for Data {
+        fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+            write!(f, "Data(value={}, id={})", self.value, self.id)
+        }
+    }
+    impl PartialEq<i64> for Data {
+        fn eq(&self, other: &i64) -> bool {
+            self.value == *other
+        }
+    }
+    impl PartialEq<Data> for i64 {
+        fn eq(&self, other: &Data) -> bool {
+            *self == other.value
         }
     }
 }
