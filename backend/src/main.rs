d6a0e17dc504ed37d873fdd5881230c01cbfa37e
ucyo <cayoglu@me.com>
2020-09-29 22:14:15 +0200

    Allow CORS from frontend server, otherwise data can not be exchanged

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 //! Main application for backend service.
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use eingang_backend::routes::*;
 use eingang_backend::{HOST, PORT};
@@ -8,7 +9,7 @@ async fn main() -> std::io::Result<()> {
     let address = format!("{}:{}", HOST, PORT);
 
     HttpServer::new(|| {
-        App::new()
+        App::new().wrap(Cors::new().allowed_origin("http://localhost:8080").finish())
             .service(index)
             .service(saving)
             .service(permanent)
