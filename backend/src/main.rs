e09c68b364d927ade797d165c95ad04ae133cf0b
ucyo <cayoglu@me.com>
2020-10-10 22:04:31 +0200

    Add new config setup method

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,8 @@
 use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use eingang_backend::routes::*;
-use eingang_backend::{FRONTEND_HOST, FRONTEND_PORT, HOST, PORT};
+use eingang_backend::{HOST, PORT, FRONTEND_HOST, FRONTEND_PORT};
+use actix_web::web;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -10,9 +11,8 @@ async fn main() -> std::io::Result<()> {
     let frontend = format!("{}:{}", FRONTEND_HOST, FRONTEND_PORT);
 
     HttpServer::new(move || {
-        App::new()
-            .wrap(Cors::new().allowed_origin(frontend.as_str()).finish())
-            .service(index)
+        App::new().wrap(Cors::new().allowed_origin(frontend.as_str()).finish())
+            .service(web::resource("/json/{value}").route(web::get().to(index)))
             .service(saving)
             .service(permanent)
             .service(serialize)
