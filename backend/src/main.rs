c546c01ea90a941594cade994ca5dbc81bc9de73
ucyo <cayoglu@me.com>
2020-10-12 23:16:26 +0200

    rustfmt

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,9 @@
 //! Main application for backend service.
 use actix_cors::Cors;
-use actix_web::{App, HttpServer, web};
-use eingang_backend::routes::*;
-use eingang_backend::{HOST, PORT, FRONTEND_HOST, FRONTEND_PORT};
 use actix_web::http::header;
+use actix_web::{web, App, HttpServer};
+use eingang_backend::routes::*;
+use eingang_backend::{FRONTEND_HOST, FRONTEND_PORT, HOST, PORT};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -11,13 +11,16 @@ async fn main() -> std::io::Result<()> {
     let frontend = format!("{}:{}", FRONTEND_HOST, FRONTEND_PORT);
 
     HttpServer::new(move || {
-        App::new().wrap(Cors::new()
-        .allowed_origin(&frontend)
-        .allowed_methods(vec!["GET", "POST", "DELETE", "PATCH"])
-        .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
-        .allowed_header(header::CONTENT_TYPE)
-        .max_age(3600)
-        .finish())
+        App::new()
+            .wrap(
+                Cors::new()
+                    .allowed_origin(&frontend)
+                    .allowed_methods(vec!["GET", "POST", "DELETE", "PATCH"])
+                    .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+                    .allowed_header(header::CONTENT_TYPE)
+                    .max_age(3600)
+                    .finish(),
+            )
             .service(web::resource("/json/{value}").route(web::get().to(index)))
             .service(saving)
             .service(permanent)
