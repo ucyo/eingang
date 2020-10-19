dc5847ef1718756dd686f1db4f5b827daa1838ab
ucyo <cayoglu@me.com>
2020-10-19 22:10:58 +0200

    Export filtering options

--- base/src/models/mod.rs
+++ base/src/models/mod.rs
@@ -5,5 +5,5 @@ pub use self::eingang::note::{Note, NoteQuery};
 pub use self::eingang::task::{Task, TaskQuery, TaskStatus};
 pub use self::eingang::thread::{Thread, TaskUuid, NoteUuid, ThreadFilter, ThreadQuery, ThreadResponse};
 pub use self::eingang::Idable;
-pub use self::eingang::journal::{JournalQuery, JournalResponse, Period};
+pub use self::eingang::journal::{JournalQuery, JournalResponse, JournalFilter, Period};
 pub use data::Data;
