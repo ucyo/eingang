ad63443ed410e9e26b5f8db1be27356d12fc3d6f
ucyo <cayoglu@me.com>
2020-10-19 21:02:32 +0200

    Move parsing from str to Timestamp from `backend` to `base`

--- base/src/models/eingang/journal.rs
+++ base/src/models/eingang/journal.rs
@@ -1,5 +1,9 @@
 use serde::{Deserialize, Serialize};
 use super::{note::Note, task::Task, thread::Thread};
+use super::Timestamp;
+use chrono::NaiveDate;
+
+const TIME: &str = "%Y-%m-%d";
 
 #[derive(Serialize, Deserialize, Debug, Default)]
 pub struct JournalQuery {
@@ -7,6 +11,37 @@ pub struct JournalQuery {
     pub before: Option<String>,
     pub during: Option<Period>,
     pub untouched: Option<Period>,
+}
+
+impl JournalQuery {
+    pub fn after_to_timestamp(&self) -> Option<Timestamp> {
+        match &self.after {
+            Some(s) => {
+                match NaiveDate::parse_from_str(s.as_str(), TIME).ok() {
+                    Some(s) => {
+                        let ndt = s.and_hms(0, 0, 1);
+                        Some(Timestamp::from_utc(ndt, chrono::Utc))
+                    },
+                    _ => None,
+                }
+            }
+            _ => None
+        }
+    }
+    pub fn before_to_timestamp(&self) -> Option<Timestamp> {
+        match &self.before {
+            Some(s) => {
+                match NaiveDate::parse_from_str(s.as_str(), TIME).ok() {
+                    Some(s) => {
+                        let ndt = s.and_hms(23, 59, 59);
+                        Some(Timestamp::from_utc(ndt, chrono::Utc))
+                    },
+                    _ => None,
+                }
+            }
+            _ => None
+        }
+    }
 }
 
 #[derive(Serialize, Default, Deserialize, Debug)]
