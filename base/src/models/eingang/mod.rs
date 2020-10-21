9b8b05aac93d02398090c02ba783d8de4bf49c24
ucyo <cayoglu@me.com>
2020-10-21 21:18:48 +0200

    Cleanup

--- base/src/models/eingang/mod.rs
+++ base/src/models/eingang/mod.rs
@@ -1,8 +1,3 @@
-//! # Note model
-//!
-//! In this crate all model and struct defintions regarding the
-//! `Note` componented are saved.
-
 pub trait Idable {
     fn get_uuid(&self) -> uuid::Uuid;
 }
@@ -14,32 +9,3 @@ pub mod meta;
 pub mod note;
 pub mod task;
 pub mod thread;
-
-// TODO Figure out ordering of methods
-// Notes should be compared by content
-// Tasks compared by content
-// threads not at all (for the moment)
-// Time and Notes.meta.lastmodified
-// Sortable by status for Tasks
-
-// use std::cmp::Ordering;
-
-// impl Ord for Meta {
-//     fn cmp(&self, other: &Self) -> Ordering {
-//         self.last_modified.cmp(&other.last_modified)
-//     }
-// }
-
-// impl PartialOrd for Meta {
-//     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-//         Some(self.last_modified.cmp(&other.last_modified))
-//     }
-// }
-
-// impl Eq for Meta {}
-
-// impl PartialEq for Meta {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.last_modified == other.last_modified
-//     }
-// }
