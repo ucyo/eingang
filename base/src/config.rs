dc84c9c0a6088229746f88bb8111d3bf3367679f
ucyo <cayoglu@me.com>
2020-11-12 22:45:24 +0100

    Add configuration support

--- base/src/config.rs
+++ base/src/config.rs
@@ -1,3 +1,5 @@
+use pulldown_cmark::Options;
+
 pub mod backend {
     pub const HOST: &str = "127.0.0.1";
     pub const PORT: &str = "8081";
@@ -10,6 +12,11 @@ pub mod backend {
 
 pub mod models {
     pub const TIME: &str = "%Y-%m-%d";
+    pub const RENDER_MARKDOWN_TABLES: bool = true;
+    pub const RENDER_MARKDOWN_FOOTNOTES: bool = false;
+    pub const RENDER_MARKDOWN_STRIKETHROUGH: bool = true;
+    pub const RENDER_MARKDOWN_TASKLISTS: bool = true;
+    pub const RENDER_MARKDOWN_SMART_PUNCTUATION: bool = false;
 }
 
 pub mod frontend {
@@ -17,4 +24,14 @@ pub mod frontend {
     pub const PORT: &str = "8080";
 
     pub const KEY: &str = "eingang.model.store";
+}
+
+pub fn get_markdown_options() -> Options {
+    let mut options = Options::empty();
+    options.set(Options::ENABLE_TABLES, models::RENDER_MARKDOWN_TABLES);
+    options.set(Options::ENABLE_FOOTNOTES, models::RENDER_MARKDOWN_FOOTNOTES);
+    options.set(Options::ENABLE_STRIKETHROUGH, models::RENDER_MARKDOWN_STRIKETHROUGH);
+    options.set(Options::ENABLE_TASKLISTS, models::RENDER_MARKDOWN_TASKLISTS);
+    options.set(Options::ENABLE_SMART_PUNCTUATION, models::RENDER_MARKDOWN_SMART_PUNCTUATION);
+    options
 }
