1434f2501db1a796c899a3b34a2884225a6bb496
ucyo <cayoglu@me.com>
2020-11-14 19:57:34 +0100

    Saving can be now persistent

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -12,9 +12,24 @@ type FetchJsonCallback<T> = Callback<FetchJsonResponse<T>>;
 pub type FetchStringResponse = Response<Result<String, Error>>;
 type FetchStringCallback = Callback<FetchStringResponse>;
 
+
 pub fn get_all_notes(callback: FetchJsonCallback<Vec<Note>>) -> FetchTask {
     let uri = format!("http://{}:{}/notes", BACKEND_HOST, BACKEND_PORT);
     let request = Request::get(uri).body(Nothing).unwrap();
+    yew::services::FetchService::fetch(request, callback).unwrap()
+}
+
+pub fn save_single_note(callback: FetchStringCallback, note_id: uuid::Uuid, obj: Json<&serde_json::Value>) -> FetchTask {
+    let uri = format!(
+        "http://{}:{}/notes/{}/update",
+        BACKEND_HOST, BACKEND_PORT, note_id
+    );
+    let request = Request::builder()
+        .method("PATCH")
+        .uri(uri)
+        .header("Content-Type", "application/json")
+        .body(obj)
+        .unwrap();
     yew::services::FetchService::fetch(request, callback).unwrap()
 }
 
