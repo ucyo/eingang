cedb3f4a915e8db86b3c6396663ea4984638a4c6
ucyo <cayoglu@me.com>
2020-11-16 21:34:24 +0100

    Use macros to create api calls

--- frontend/src/api.rs
+++ frontend/src/api.rs
@@ -12,43 +12,86 @@ type FetchJsonCallback<T> = Callback<FetchJsonResponse<T>>;
 pub type FetchStringResponse = Response<Result<String, Error>>;
 type FetchStringCallback = Callback<FetchStringResponse>;
 
-// TODO Using macros to generate api:
-// https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=5aff7f6358ae521cd4ff8aa6921b0102
-
-pub fn get_all_notes(callback: FetchJsonCallback<Vec<Note>>) -> FetchTask {
-    let uri = format!("http://{}:{}/notes", BACKEND_HOST, BACKEND_PORT);
-    let request = Request::get(uri).body(Nothing).unwrap();
-    yew::services::FetchService::fetch(request, callback).unwrap()
+/// This macro takes four arguments is defined for none uuid notes.
+/// $func_name: Name of the function to be generated
+/// $callback_ty: Type of the callback
+/// $uri: URL of the backend with 2 braces
+/// $method: HTTP protocol
+macro_rules! create_api {
+    ($func_name:ident,
+        $callback_type:ty,
+        $uri:expr,
+        $method:ident
+       ) => {
+           pub fn $func_name(callback: $callback_type) -> FetchTask {
+               let uri = format!($uri, BACKEND_HOST, BACKEND_PORT);
+               let request = Request::builder()
+                    .method(stringify!($method))
+                    .uri(uri)
+                    .body(Nothing)
+                    .unwrap();
+               yew::services::FetchService::fetch(request, callback).unwrap()
+           }
+       };
 }
 
-pub fn save_single_note(callback: FetchStringCallback, note_id: uuid::Uuid, obj: Json<&serde_json::Value>) -> FetchTask {
-    let uri = format!(
-        "http://{}:{}/notes/{}/update",
-        BACKEND_HOST, BACKEND_PORT, note_id
-    );
-    let request = Request::builder()
-        .method("PATCH")
-        .uri(uri)
-        .header("Content-Type", "application/json")
-        .body(obj)
-        .unwrap();
-    yew::services::FetchService::fetch(request, callback).unwrap()
+macro_rules! create_single_api_json {
+    ($func_name:ident,
+        $callback_type:ty,
+        $uri:expr,
+        $method:ident
+       ) => {
+           pub fn $func_name(callback: $callback_type, id: uuid::Uuid, obj: Json<&serde_json::Value>) -> FetchTask {
+               let uri = format!($uri, BACKEND_HOST, BACKEND_PORT, id);
+               let request = Request::builder()
+                    .method(stringify!($method))
+                    .uri(uri)
+                    .header("Content-Type", "application/json")
+                    .body(obj)
+                    .unwrap();
+               yew::services::FetchService::fetch(request, callback).unwrap()
+           }
+       };
 }
 
-pub fn get_single_note(callback: FetchJsonCallback<Note>,  note_id: uuid::Uuid) -> FetchTask {
-    let uri = format!(
-        "http://{}:{}/notes/{}",
-        BACKEND_HOST, BACKEND_PORT, note_id
-    );
-    let request = Request::get(uri).body(Nothing).unwrap();
-    yew::services::FetchService::fetch(request, callback).unwrap()
+macro_rules! create_api_uuid {
+    ($func_name:ident,
+        $callback_type:ty,
+        $uri:expr,
+        $method:ident
+       ) => {
+           pub fn $func_name(callback: $callback_type, id: uuid::Uuid) -> FetchTask {
+               let uri = format!($uri, BACKEND_HOST, BACKEND_PORT, id);
+               let request = Request::builder()
+                    .method(stringify!($method))
+                    .uri(uri)
+                    .body(Nothing)
+                    .unwrap();
+               yew::services::FetchService::fetch(request, callback).unwrap()
+           }
+       };
 }
 
-pub fn delete_single_note(callback: FetchStringCallback, note_id: uuid::Uuid) -> FetchTask {
-    let uri = format!(
-        "http://{}:{}/notes/{}/delete",
-        BACKEND_HOST, BACKEND_PORT, note_id
-    );
-    let request = Request::delete(uri).body(Nothing).unwrap();
-    yew::services::FetchService::fetch(request, callback).unwrap()
+create_single_api_json!{
+    save_single_note, FetchStringCallback, "http://{}:{}/notes/{}/update",
+    PATCH
+}
+
+create_api!{
+    get_all_notes, FetchJsonCallback<Vec<Note>>, "http://{}:{}/notes",
+    GET
+}
+
+create_api_uuid!{
+    get_single_note,
+    FetchJsonCallback<Note>,
+    "http://{}:{}/notes/{}",
+    GET
+}
+
+create_api_uuid!{
+    delete_single_note,
+    FetchStringCallback,
+    "http://{}:{}/notes/{}/delete",
+    DELETE
 }
